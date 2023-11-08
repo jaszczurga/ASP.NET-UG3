852f9ff1db7f11674cb633374cdd27067a098bc4
jaszczurga <goskar2k@gmail.com>
2023-11-08 23:36:54 +0100

    Add project files.

---
+++ CompanyOG/Models/CompanyContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyOG.Models
+{
+    public class CompanyContext : DbContext
+    {
+        //ctor
+        public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
+        {
+            
+        }
+        //prop
+        public DbSet<CountryRegion> CountryRegions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // c=> c.Code zamiast "Code"
+            modelBuilder.Entity<CountryRegion>().HasKey(c => c.Code);
+            modelBuilder
+                .Entity<CountryRegion>()
+                .Property(c=>c.Code)
+                .HasColumnName("CountryRegionCode");
+            modelBuilder.Entity<CountryRegion>().ToTable("CountryRegion", "Person");
+
+        }
+
+    }
+}
---
+++ CompanyOG/Models/CountryRegion.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyOG.Models
+{
+    public class CountryRegion
+    {
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
